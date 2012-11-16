b80309ce9fbbbfd30cd69ca219e2cea05e8b4455
HosamMohd <hosamkareem@gmail.com>
2012-11-16 14:51:12 -0700

    created main activity layout

--- gen/edu/mines/csci498/geosocial/R.java
+++ gen/edu/mines/csci498/geosocial/R.java
@@ -15,7 +15,9 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070000;
+        public static final int details=0x7f070000;
+        public static final int menu_settings=0x7f070002;
+        public static final int name=0x7f070001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -25,8 +27,8 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f040000;
-        public static final int hello_world=0x7f040001;
         public static final int menu_settings=0x7f040002;
+        public static final int status=0x7f040001;
         public static final int title_activity_main=0x7f040003;
     }
     public static final class style {
