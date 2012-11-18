d6370212bcc7a7a0ecaf7c395fc2f970db4d7abe
HosamMohd <hosamkareem@gmail.com>
2012-11-17 20:49:22 -0700

    Creating Request Activity, Connect items from the menu to both activities (Send Friend Request & Main items)

--- gen/edu/mines/csci498/geosocial/R.java
+++ gen/edu/mines/csci498/geosocial/R.java
@@ -15,15 +15,19 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
+        public static final int button1=0x7f070002;
         public static final int details=0x7f070000;
-        public static final int menu_friends_list=0x7f070003;
-        public static final int menu_main=0x7f070005;
-        public static final int menu_send_request=0x7f070004;
-        public static final int menu_settings=0x7f070002;
+        public static final int menu_friends_list=0x7f070006;
+        public static final int menu_main=0x7f070008;
+        public static final int menu_send_request=0x7f070007;
+        public static final int menu_settings=0x7f070005;
         public static final int name=0x7f070001;
+        public static final int phone_field=0x7f070004;
+        public static final int tableRow1=0x7f070003;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int request_activity=0x7f030001;
     }
     public static final class menu {
         public static final int activity_main=0x7f060000;
@@ -33,8 +37,10 @@ public final class R {
         public static final int app_name=0x7f040000;
         public static final int item_all_friends=0x7f040004;
         public static final int item_main=0x7f040005;
-        public static final int item_send_request=0x7f040003;
         public static final int menu_settings=0x7f040002;
+        public static final int phone_number=0x7f040007;
+        public static final int send_button=0x7f040008;
+        public static final int send_request=0x7f040003;
         public static final int status=0x7f040001;
         public static final int title_activity_main=0x7f040006;
     }
--- src/edu/mines/csci498/geosocial/RequestActivity.java
+++ src/edu/mines/csci498/geosocial/RequestActivity.java
@@ -1,31 +1,32 @@
 package edu.mines.csci498.geosocial;
 
-import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends Activity {
+public class RequestActivity extends Activity{
 
-    @Override
+	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.request_activity);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-    	getMenuInflater().inflate(R.menu.activity_main, menu);
+    	getMenuInflater().inflate(R.menu.request_options, menu);
         return true;
     }
     
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-    	if(item.getItemId() == R.id.menu_send_request){
-    		startActivity(new Intent(this, RequestActivity.class));
+    	if(item.getItemId() == R.id.menu_main){
+    		startActivity(new Intent(this, MainActivity.class));
     	}
     	
     	return super.onOptionsItemSelected(item);
     }
+
 }
