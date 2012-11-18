151addcc502d5289d948e3abd88da347e1618368
HosamMohd <hosamkareem@gmail.com>
2012-11-17 19:48:45 -0700

    Adding items to the menu & Creating another menu for the second activity (activity_main.xml - request_option.xml)

--- gen/edu/mines/csci498/geosocial/R.java
+++ gen/edu/mines/csci498/geosocial/R.java
@@ -16,6 +16,9 @@ public final class R {
     }
     public static final class id {
         public static final int details=0x7f070000;
+        public static final int menu_friends_list=0x7f070003;
+        public static final int menu_main=0x7f070005;
+        public static final int menu_send_request=0x7f070004;
         public static final int menu_settings=0x7f070002;
         public static final int name=0x7f070001;
     }
@@ -24,12 +27,16 @@ public final class R {
     }
     public static final class menu {
         public static final int activity_main=0x7f060000;
+        public static final int request_options=0x7f060001;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
+        public static final int item_all_friends=0x7f040004;
+        public static final int item_main=0x7f040005;
+        public static final int item_send_request=0x7f040003;
         public static final int menu_settings=0x7f040002;
         public static final int status=0x7f040001;
-        public static final int title_activity_main=0x7f040003;
+        public static final int title_activity_main=0x7f040006;
     }
     public static final class style {
         public static final int AppTheme=0x7f050000;
